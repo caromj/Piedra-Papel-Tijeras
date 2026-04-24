4f25c8dda8c65ed903fb5283d53af68ee5382127
Carolina <caromorenoj@gmail.com>
2026-04-24 10:53:18 +0200

    quitando base de datos

--- index.js
+++ index.js
@@ -3,82 +3,31 @@ import { createServer } from 'node:http';
 import { fileURLToPath } from 'node:url';
 import { dirname, join } from 'node:path';
 import { Server } from 'socket.io';
-import sqlite3 from 'sqlite3';
-import { open } from 'sqlite';
-import { availableParallelism } from 'node:os';
-import cluster from 'node:cluster';
-import { createAdapter, setupPrimary } from '@socket.io/cluster-adapter';
 
-if (cluster.isPrimary) { //esto hace crear una variable  del entorno port 3000,3001,3003....
-  const numCPUs = availableParallelism();
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork({
-      PORT: 3000 + i
-    });
-  }
 
-  setupPrimary();
-} else {
-  const db = await open({
-    filename: 'chat.db',
-    driver: sqlite3.Database
-  });
-
-  await db.exec(`
-    CREATE TABLE IF NOT EXISTS messages (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      client_offset TEXT UNIQUE,
-      content TEXT
-    );
-  `);
-
-  const app = express();
-  const server = createServer(app);
-  const io = new Server(server, {
-    connectionStateRecovery: {},
-    adapter: createAdapter()
-  });
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
 
   const __dirname = dirname(fileURLToPath(import.meta.url));
 
-  app.get('/', (req, res) => {
+  app.get('/', (req, res) => { 
     res.sendFile(join(__dirname, 'index.html'));
   });
 
-  io.on('connection', async (socket) => { // enviaremos todos los mensajes incluso al emisor 
-    socket.on('chat message', async (msg, clientOffset, callback) => {
-      let result;
-      try {
-        result = await db.run('INSERT INTO messages (content, client_offset) VALUES (?, ?)', msg, clientOffset);
-      } catch (e) {
-        if (e.errno === 19 /* SQLITE_CONSTRAINT */ ) {
-          callback();
-        } else {
-          // nada que hacer, solo dejar que el cliente reintente
-        }
-        return;
-      }
-      io.emit('chat message', msg, result.lastID);
-      callback();
-    });
 
-    if (!socket.recovered) {
-      try {
-        await db.each('SELECT id, content FROM messages WHERE id > ?',
-          [socket.handshake.auth.serverOffset || 0],
-          (_err, row) => {
-            socket.emit('chat message', row.content, row.id);
-          }
-        )
-      } catch (e) {
-        // algo salió mal
-      }
-    }
+
+io.on('connection', (socket) => {
+  console.log("Jugador conectado:", socket.id);
+
+  // Ejemplo simple de chat
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
   });
+});
 
-  const port = process.env.PORT;
+const PORT = 3000;
 
-  server.listen(port, () => {
-    console.log(`servidor corriendo en http://localhost:${port}`);
-  });
-}
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Servidor corriendo en http://TU_IP_LOCAL:${PORT}`);
+});
